e690ba2c3bc9a2dd85028efa02ec1b55fe5ace4b
Elizabethyonas <elizabethyonas700@gmail.com>
2025-04-25 18:01:25 +0300

    fix: variable name

--- main.go
+++ main.go
@@ -24,13 +24,13 @@ func main() {
 	}
 	ensureUploadDirExists()
 
-	r := gin.Default()
-	r.Static("/uploads", "./uploads")
-	router.SetupRoutes(r, db)
+	app := gin.Default()
+	app.Static("/uploads", "./uploads")
+	router.SetupRoutes(app, db)
 
 	port := ":8080"
 	log.Printf("Server running on http://localhost%s", port)
-	err = r.Run(port)
+	err = app.Run(port)
 	if err != nil {
 		log.Fatalf("Server failed: %s", err)
 	}
